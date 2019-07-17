2707e691041748dd09e37aad1425b2020b533265
liupeng <37412816+liupengzhouyi@users.noreply.github.com>
2019-07-17 14:28:06 +0800

    新公司类型模型 V 1.0.3

---
+++ src/processData/getData/Model/newCompanyInformation.java
@@ -0,0 +1,118 @@
+package processData.getData.Model;
+
+public class newCompanyInformation {
+
+    // 企业名称
+    private String enterpriseName;
+
+    // 联系人
+    private String contact;
+
+    // 联系电话
+    private String call;
+
+    // 所属行业
+    private String industry;
+
+    // 企业性质
+    private String theNatureOfTheBusiness;
+
+    // 企业规模
+    private String sizeOfTheEnterprise;
+
+    // 注册日期
+    private String registrationDate;
+
+    // 企业福利
+    private String corporateBenefits;
+
+    // 企业地址
+    private String businessAddress;
+
+    public String getEnterpriseName() {
+        return enterpriseName;
+    }
+
+    public void setEnterpriseName(String enterpriseName) {
+        this.enterpriseName = enterpriseName;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getCall() {
+        return call;
+    }
+
+    public void setCall(String call) {
+        this.call = call;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public String getTheNatureOfTheBusiness() {
+        return theNatureOfTheBusiness;
+    }
+
+    public void setTheNatureOfTheBusiness(String theNatureOfTheBusiness) {
+        this.theNatureOfTheBusiness = theNatureOfTheBusiness;
+    }
+
+    public String getSizeOfTheEnterprise() {
+        return sizeOfTheEnterprise;
+    }
+
+    public void setSizeOfTheEnterprise(String sizeOfTheEnterprise) {
+        this.sizeOfTheEnterprise = sizeOfTheEnterprise;
+    }
+
+    public String getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(String registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public String getCorporateBenefits() {
+        return corporateBenefits;
+    }
+
+    public void setCorporateBenefits(String corporateBenefits) {
+        this.corporateBenefits = corporateBenefits;
+    }
+
+    public String getBusinessAddress() {
+        return businessAddress;
+    }
+
+    public void setBusinessAddress(String businessAddress) {
+        this.businessAddress = businessAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "newCompanyInformation{" +
+                "enterpriseName='" + enterpriseName + '\'' +
+                ", contact='" + contact + '\'' +
+                ", call='" + call + '\'' +
+                ", industry='" + industry + '\'' +
+                ", theNatureOfTheBusiness='" + theNatureOfTheBusiness + '\'' +
+                ", sizeOfTheEnterprise='" + sizeOfTheEnterprise + '\'' +
+                ", registrationDate='" + registrationDate + '\'' +
+                ", corporateBenefits='" + corporateBenefits + '\'' +
+                ", businessAddress='" + businessAddress + '\'' +
+                '}';
+    }
+}
